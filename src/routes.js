2869b20b3d3c17f8c57cc2f94e06d6c5cad79c43
Ladinar <ladinarnanda@gmail.com>
2022-02-05 23:20:42 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -1,53 +1,12 @@
 import React from 'react'
 import { lazy } from 'react'
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
-const Typography = React.lazy(() => import('./views/theme/typography/Typography'))
-
-// Base
-const Accordion = React.lazy(() => import('./views/base/accordion/Accordion'))
-const Breadcrumbs = React.lazy(() => import('./views/base/breadcrumbs/Breadcrumbs'))
-const Cards = React.lazy(() => import('./views/base/cards/Cards'))
-const Carousels = React.lazy(() => import('./views/base/carousels/Carousels'))
-const Collapses = React.lazy(() => import('./views/base/collapses/Collapses'))
-const ListGroups = React.lazy(() => import('./views/base/list-groups/ListGroups'))
-const Navs = React.lazy(() => import('./views/base/navs/Navs'))
-const Paginations = React.lazy(() => import('./views/base/paginations/Paginations'))
-const Placeholders = React.lazy(() => import('./views/base/placeholders/Placeholders'))
-const Popovers = React.lazy(() => import('./views/base/popovers/Popovers'))
-const Progress = React.lazy(() => import('./views/base/progress/Progress'))
-const Spinners = React.lazy(() => import('./views/base/spinners/Spinners'))
-const Tables = React.lazy(() => import('./views/base/tables/AllProducts'))
-const Tooltips = React.lazy(() => import('./views/base/tooltips/Tooltips'))
-
 //Products
 const AllProducts = React.lazy(() => import('./views/products/all-products/AllProducts'))
 const EditProducts = React.lazy(() => import('./views/products/edit-products/EditProducts'))
 const AddProducts = React.lazy(() => import('./views/products/add-products/AddProducts'))
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/theme', name: 'Theme', component: Colors, exact: true },
-  { path: '/theme/colors', name: 'Colors', component: Colors },
-  { path: '/theme/typography', name: 'Typography', component: Typography },
-  { path: '/base', name: 'Base', component: Cards, exact: true },
-  { path: '/base/accordion', name: 'Accordion', component: Accordion },
-  { path: '/base/breadcrumbs', name: 'Breadcrumbs', component: Breadcrumbs },
-  { path: '/base/cards', name: 'Cards', component: Cards },
-  { path: '/base/carousels', name: 'Carousel', component: Carousels },
-  { path: '/base/collapses', name: 'Collapse', component: Collapses },
-  { path: '/base/list-groups', name: 'List Groups', component: ListGroups },
-  { path: '/base/navs', name: 'Navs', component: Navs },
-  { path: '/base/paginations', name: 'Paginations', component: Paginations },
-  { path: '/base/placeholders', name: 'Placeholders', component: Placeholders },
-  { path: '/base/popovers', name: 'Popovers', component: Popovers },
-  { path: '/base/progress', name: 'Progress', component: Progress },
-  { path: '/base/spinners', name: 'Spinners', component: Spinners },
-  { path: '/base/tables', name: 'Tables', component: Tables },
-  { path: '/base/tooltips', name: 'Tooltips', component: Tooltips },
-  { path: '/products', name: 'Produk', component: AllProducts, exact: true },
   { path: '/products/all-products', name: 'Produk', component: AllProducts },
   { path: '/products/edit-products/:id', name: 'Edit Produk', component: EditProducts },
   { path: '/products/add-products', name: 'Tambah Produk', component: AddProducts },
